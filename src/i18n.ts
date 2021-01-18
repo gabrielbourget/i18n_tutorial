6feaaebbce27582a604716ad681353904099471c
Gabriel Bourget <gaboxa@syntronic.com>
2021-01-17 23:42:31 -0500

    work

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,23 @@
 // -> Beyond Codebase
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import Backend from "i18next-xhr-backend";
+import HttpApi from "i18next-http-backend"; // -> Tutorial I followed used i18next-xhr-backend but it has been deprecated with advice to use this one now.
 // -> Within Codebase
 import { TRANSLATIONS_BACKEND_URL } from "./constants";
 
-// -> Original tutorial can be found here: https://www.robinwieruch.de/react-internationalization
+// - TO LOOK INTO
+// -> Events such as onInitialized, onLanguageChanged
+// -> i18next-react-postprocessor to embed React elements inside translation strings
 
+// - KEY QUESTIONS FOR ENTERPRISE-LEVEL PROJECTS
+// -> How does the translation process work?
+// -> How do the source files get to the translators and back?
+// -> How do you keep track which parts are already translated and which parts not - and additional are all target languages fully translated?
+// -> How you deploy new languages after release?
+// -> How do you handle versioning?
+// -> How you update / fix typos in translations after deployment?
+
+// -> Original tutorial can be found here: https://www.robinwieruch.de/react-internationalization
 // -> All config options can be found at https://www.i18next.com/overview/configuration-options
 const i18nInitConfig = {
   debug: true,
@@ -25,7 +36,7 @@ const i18nInitConfig = {
 };
 
 i18n
-  .use(Backend) // -> More info: https://github.com/i18next/i18next-xhr-backend
+  .use(HttpApi) // -> More info: https://github.com/i18next/i18next-xhr-backend
   .use(initReactI18next) // -> Connect with React
   .init({ ...i18nInitConfig });
 
